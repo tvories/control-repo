95e789aa904fd4a366d7c02b3b02c327ea06ae6a
Taylor Vories <taylor@tmtech.me>
2017-10-11 10:48:57 -0600

    re-added chocolatey to the package update

--- site/profile/manifests/chocolatey.pp
+++ site/profile/manifests/chocolatey.pp
@@ -1,7 +1,11 @@
 class profile::chocolatey {
   include chocolatey
 
+  package { 'chocolatey':
+    ensure      =>  'latest',
+  }
+
   package {'git':
-    ensure => 'present',
+    ensure => 'latest',
   }
 }
