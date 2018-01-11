8ac598018049eefd15158140caf413cc749f0588
Taylor Vories <taylor@tmtech.me>
2018-01-11 15:47:02 -0700

    Fixed typo on class name

--- site/profile/manifests/base/desktop_experience.pp
+++ site/profile/manifests/base/desktop_experience.pp
@@ -1,7 +1,7 @@
 # == Class: profile::base::desktop_experience
 class profile::base::desktop_experience (
 ) {
-  class { 'firewallprofile_win':
+  class { 'showhidden':
     show_file_ext_version       => '1,0,0',
     show_hidden_folders_version => '1,0,0',
   }
