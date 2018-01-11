a98fde80357083b0e9071b34b5e80f2aa7aebca1
Taylor Vories <taylor@tmtech.me>
2018-01-11 16:58:13 -0700

    trying out verssion changes with desktop experience

--- site/profile/manifests/base/desktop_experience.pp
+++ site/profile/manifests/base/desktop_experience.pp
@@ -2,7 +2,9 @@
 class profile::base::desktop_experience (
 ) {
   class { 'showhidden':
-    show_file_ext_version       => '1,0,0',
-    show_hidden_folders_version => '1,0,0',
+    show_file_ext_version       => '1,0,1',
+    show_hidden_folders_version => '1,0,1',
+    show_file_ext               => false,
+    show_hidden_folders         => false,
   }
 }
