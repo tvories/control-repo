196c871b554a41dd286446081724db7d8a154e91
Taylor Vories <taylor.vories@davita.com>
2018-08-15 12:28:15 -0600

    Cleaning up and refactoring

--- site/role/manifests/member_server.pp
+++ site/role/manifests/member_server.pp
@@ -1,3 +1,19 @@
 class role::member_server {
   include profile::base
+  case $os.family {
+    'linux' {
+      include profile::linux::base
+    }
+    'windows' {
+      include profile::windows::base
+    }
+  }
+
+
+
+  if(os.family = 'linux') {
+    include profile::linux::base
+  } else if (os.family = 'windows') {
+    include profile::windows::base
+  }
 }
