764b1f97163a937a691793d2cca6c022b5e7cdd0
Taylor Vories <taylor.vories@davita.com>
2018-08-16 11:58:51 -0600

    fixed syntax

--- site/role/manifests/member_server.pp
+++ site/role/manifests/member_server.pp
@@ -1,19 +1,22 @@
 class role::member_server {
   include profile::base
-  case $os.family {
-    'linux' {
+  case $facts['os']['family'] {
+    'linux': {
       include profile::linux::base
     }
-    'windows' {
+    'windows': {
       include profile::windows::base
+    }
+    default: {
+      include profile::base
     }
   }
 
 
 
-  if(os.family = 'linux') {
-    include profile::linux::base
-  } else if (os.family = 'windows') {
-    include profile::windows::base
-  }
+  # if(os.family = 'linux') {
+  #   include profile::linux::base
+  # } else if (os.family = 'windows') {
+  #   include profile::windows::base
+  # }
 }
