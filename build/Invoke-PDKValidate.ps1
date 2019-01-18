129a3dd1f48bdd501f373b161df8ced0e734c573
Taylor Vories <taylor@tmtech.me>
2019-01-18 09:08:57 -0700

    Trying to fix some pdk issues

--- build/Invoke-PDKValidate.ps1
+++ build/Invoke-PDKValidate.ps1
@@ -7,11 +7,6 @@ Param(
 
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
-$psModule = 'PuppetDevelopmentKit'
 
-if (Get-Module $psModule -ListAvailable) {
-    Import-Module -Name $psModule
-    pdk validate $test
-} else {
-    Write-Error "$psModule not installed on system"
-}
+Import-Module -Name 'PuppetDevelopmentKit'
+pdk validate $test
